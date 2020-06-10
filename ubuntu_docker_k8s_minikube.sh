1e9b3163d7cbc40c1801b874fa780d22cc4e2084
Robert Brewitz <hello@robertbrewitz.com>
2020-06-11 00:15:16 +0200

    UPDATES ubuntu_docer_k8s_minikube.sh

--- ubuntu_docker_k8s_minikube.sh
+++ ubuntu_docker_k8s_minikube.sh
@@ -3,14 +3,14 @@
 # https://docs.docker.com/engine/install/ubuntu/
 echo "Setting up apt Docker repository"
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
    "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
    $(lsb_release -cs) \
    stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 
 # https://docs.docker.com/compose/install/
 echo "Installing docker-compose"
@@ -31,7 +31,7 @@ sudo mv ./kompose /usr/local/bin/kompose
 
 # https://help.ubuntu.com/community/KVM/Installation#Installation_of_KVM
 echo "Installing kvm for minikube"
-sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils
+sudo apt-get install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils -y
 sudo adduser `id -un` libvirt
 sudo adduser `id -un` kvm
 
