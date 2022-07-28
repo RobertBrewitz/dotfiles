d039f40d17d27bbe5c5ca97f432ac4be4dc528a4
Robert Brewitz <robert.brewitz@viaplaygroup.com>
2022-07-28 10:38:09 +0200

    UPDATES telescope mappings

--- nvim/after/plugin/keymap/telescope.lua
+++ nvim/after/plugin/keymap/telescope.lua
@@ -1,6 +1,29 @@
 local Remap = require("jrbb.keymap")
 local nnoremap = Remap.nnoremap
+local actions = require("telescope.actions")
 
+require("telescope").setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_better,
+      },
+      n = {
+        ["<S-Tab>"] = actions.toggle_selection + actions.move_selection_worse,
+        ["<Tab>"] = actions.toggle_selection + actions.move_selection_better,
+      }
+    },
+  }
+}
+
+nnoremap("<S-Tab>", function ()
+  require('telescope.builtin').prev()
+end)
+nnoremap("<Tab>", function ()
+  require('telescope.builtin').next()
+end)
 nnoremap("<C-p>", function()
   require('telescope.builtin').git_files()
 end)
