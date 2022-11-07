7202cdc072eed6b77b1b9cb89792272351273e91
Robert Brewitz <hello@robertbrewitz.com>
2022-11-07 21:21:52 +0100

    ADDS leader-f to fold curly braces and wrap

--- nvim/lua/jrbb/config.lua
+++ nvim/lua/jrbb/config.lua
@@ -32,7 +32,7 @@ vim.opt.expandtab = true
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
-vim.opt.wrap = false
+vim.opt.wrap = true
 vim.opt.startofline = false
 
 -- presentation
