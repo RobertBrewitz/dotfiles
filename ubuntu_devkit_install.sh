cbe67c72c9c43025003a02152e77613b77681450
Robert Brewitz <hello@robertbrewitz.com>
2023-03-30 08:56:44 +0200

    ADDS echo "DONE" to ubuntu_devkit_install.sh script

--- ubuntu_devkit_install.sh
+++ ubuntu_devkit_install.sh
@@ -12,6 +12,7 @@ echo \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 echo "Manage Docker as a non-root user"
 if [ ! $(getent group docker) ]; then
   sudo groupadd docker
@@ -51,3 +52,5 @@ echo "Installing helm"
 curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
 chmod 700 get_helm.sh
 ./get_helm.sh
+
+echo "DONE"
