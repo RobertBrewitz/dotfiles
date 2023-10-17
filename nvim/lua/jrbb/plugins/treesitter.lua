39d2548009d19a30a5a9ad689c94155e419e2f31
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2023-10-17 20:48:04 +0200

    ADDS syntax highlighting for wgsl

--- nvim/lua/jrbb/plugins/treesitter.lua
+++ nvim/lua/jrbb/plugins/treesitter.lua
@@ -2,8 +2,10 @@ return {
   "nvim-treesitter/nvim-treesitter",
   run = ":TSUpdate",
   config = function()
+    vim.filetype.add({extension = {wgsl = "wgsl"}})
+
     require'nvim-treesitter.configs'.setup {
-      ensure_installed = { "typescript", "javascript", "tsx", "rust", "markdown", "lua" },
+      ensure_installed = { "typescript", "javascript", "tsx", "rust", "markdown", "lua", "wgsl" },
       sync_install = false,
       auto_install = true,
       highlight = {
