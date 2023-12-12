6cf7ff6f7d3507670aa430aabb6cf2ea2ced8394
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2023-12-12 22:09:12 +0100

    SEPARATES docker and k8s installation scripts

---
+++ ubuntu_docker_install.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# https://docs.docker.com/engine/install/ubuntu/
+echo "Setting up apt Docker repository"
+sudo apt-get update
+sudo apt-get install ca-certificates curl gnupg -y
+sudo mkdir -m 0755 -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Manage Docker as a non-root user"
+if [ ! $(getent group docker) ]; then
+  sudo groupadd docker
+fi
+if ! id -nG "$USER" | grep -qw "docker"; then
+  sudo usermod -aG docker $USER
+  newgrp docker
+fi
+
+echo "Make Docker use host machine's DNS"
+sudo apt-get install dnsmasq
+sudo touch /etc/resolvconf/resolv.conf.d/tail
+sudo echo "nameserver 172.17.0.1" > /etc/resolvconf/resolv.conf.d/tail
+sudo touch /etc/NetworkManager/dnsmasq.d/docker.conf
+sudo tee -a /etc/NetworkManager/dnsmasq.d/docker.conf <<EOF
+interface=lo
+interface=tun0
+interface=docker0
+listen-address=172.17.0.1
+bind-dynamic
+EOF
+
+echo "DONE"
