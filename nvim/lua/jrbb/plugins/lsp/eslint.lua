525ef50d119cb84843d0133bc19fb4170e5438be
Robert Brewitz Borg <hello@robertbrewitz.com>
2023-12-13 10:39:07 +0100

    ADDS eslint to lsp

---
+++ nvim/lua/jrbb/plugins/lsp/eslint.lua
@@ -0,0 +1,17 @@
+return {
+  {
+    'mantoni/eslint_d',
+    dependencies = { 'neovim/nvim-lspconfig', 'hrsh7th/cmp-nvim-lsp' },
+    ft = { 'javascript', 'javascriptreact', 'typescript', 'typescriptreact' },
+    opts = function()
+      return {
+        capabilities = require('cmp_nvim_lsp').default_capabilities(
+          vim.lsp.protocol.make_client_capabilities()
+        ),
+      }
+    end,
+    config = function(_, opts)
+      require('lspconfig').eslint.setup(opts)
+    end,
+  }
+}
