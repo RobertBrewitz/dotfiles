d797741079eb38aa02ba30d096718c23e62cc2fe
Robert Brewitz Borg <hello@robertbrewitz.com>
2024-02-01 15:48:52 +0100

    ADDS url open to neovim

--- nvim/lua/jrbb/plugins/utils.lua
+++ nvim/lua/jrbb/plugins/utils.lua
@@ -8,4 +8,23 @@ return {
   {
     "editorconfig/editorconfig-vim",
   },
+  {
+    "sontungexpt/url-open",
+    branch = "mini",
+    event = "VeryLazy",
+    cmd = "URLOpenUnderCursor",
+    config = function()
+      local lspconfig = require("lspconfig")
+      local Remap = require("jrbb.keymap")
+      local nnoremap = Remap.nnoremap
+
+      vim.keymap.set("n", "go", "<esc>:URLOpenUnderCursor<cr>")
+
+      local status_ok, url_open = pcall(require, "url-open")
+      if not status_ok then
+        return
+      end
+      url_open.setup ({})
+    end,
+  },
 }
