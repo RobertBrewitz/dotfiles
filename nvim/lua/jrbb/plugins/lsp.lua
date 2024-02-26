57a4f3b0c8e1d4f6d6395ec4fa0858909dc96179
Robert Brewitz Borg <hello@robertbrewitz.com>
2024-02-27 00:35:06 +0100

    UPDATES lsp mappings

--- nvim/lua/jrbb/plugins/lsp.lua
+++ nvim/lua/jrbb/plugins/lsp.lua
@@ -1,33 +1,66 @@
 return {
   {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     config = function()
-      local lspconfig = require("lspconfig")
-      local Remap = require("jrbb.keymap")
-      local nnoremap = Remap.nnoremap
+      local lspconfig = require('lspconfig')
+      local Remap = require('jrbb.keymap')
+      local nmap = Remap.nmap
+      local vmap = Remap.vmap
 
       vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(
         vim.lsp.handlers.hover, {
-          border = "single"
+          border = 'single'
         }
       )
 
       vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(
         vim.lsp.handlers.signatureHelp, {
-          border = "single"
+          border = 'single'
         }
       )
 
       lspconfig.gopls.setup({})
-      lspconfig.lua_ls.setup({})
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = {
+              globals = { 'vim' }
+            }
+          }
+        }
+      })
 
-      nnoremap("<leader>gd", function() vim.lsp.buf.definition() end)
-      nnoremap("<leader>d", function() vim.lsp.buf.hover() end)
-      nnoremap("<leader>gi", function() vim.lsp.buf.implementation() end)
-      nnoremap("<leader>gs", function() vim.lsp.buf.signature_help() end)
-      nnoremap("<leader>gy", function() vim.lsp.buf.type_definition() end)
-      nnoremap("<leader>gr", function() vim.lsp.buf.references() end)
-      nnoremap("<leader>rn", function() vim.lsp.buf.rename() end)
-    end,
+      nmap('<space>of', vim.diagnostic.open_float)
+      nmap('<leader>p', vim.diagnostic.goto_prev)
+      nmap('<leader>n', vim.diagnostic.goto_next)
+      nmap('<space>ql', vim.diagnostic.setloclist)
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(ev)
+          vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          local opts = { buffer = ev.buf }
+
+          nmap('<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+          nmap('<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          nmap('<space>wl', function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
+
+          nmap('<leader>la', function() vim.lsp.buf.code_action() end, opts)
+          vmap('<leader>la', function() vim.lsp.buf.code_action() end, opts)
+          nmap('<leader>d', function() vim.lsp.buf.hover() end, opts)
+
+          nmap('<leader>gd', function() vim.lsp.buf.definition() end, opts)
+          nmap('<leader>li', function() vim.lsp.buf.implementation() end, opts)
+          nmap('<leader>lsh', function() vim.lsp.buf.signature_help() end, opts)
+          nmap('<leader>ltd', function() vim.lsp.buf.type_definition() end, opts)
+          nmap('<leader>lr', function() vim.lsp.buf.references() end, opts)
+          nmap('<leader>ld', function() vim.lsp.buf.declaration() end, opts)
+
+          nmap('<leader>rn', function() vim.lsp.buf.rename() end, opts)
+          nmap('<space>lf', function() vim.lsp.buf.format { async = true } end, opts)
+        end
+      })
+    end
   },
 }
