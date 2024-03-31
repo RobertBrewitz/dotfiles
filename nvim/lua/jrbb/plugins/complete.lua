db02a47df1772bf4710106e853e49dbc63b00fd3
Robert Brewitz Borg <hello@robertbrewitz.com>
2024-03-31 17:38:22 +0200

    CHANGES mapping abort mapping

--- nvim/lua/jrbb/plugins/complete.lua
+++ nvim/lua/jrbb/plugins/complete.lua
@@ -15,7 +15,6 @@ return {
       local winhighlight = {
         winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder,CursorLine:PmenuSel",
       }
-
       cmp.setup({
         formatting = {
           format = function(_, vim_item)
@@ -28,7 +27,7 @@ return {
           ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
           ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
           ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-          ["<esc>"] = cmp.mapping.abort(),
+          ["<C-e>"] = cmp.mapping(cmp.mapping.abort(), { "i", "c" }),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
         },
