e5abcd91200da5c49c15187fb29010d741c2b551
Robert Brewitz Borg <hello@robertbrewitz.com>
2024-03-31 17:25:47 +0200

    CLEANUP nvim complete config

--- nvim/lua/jrbb/plugins/complete.lua
+++ nvim/lua/jrbb/plugins/complete.lua
@@ -3,20 +3,11 @@ return {
     "hrsh7th/cmp-nvim-lsp",
   },
   {
-    "hrsh7th/vim-vsnip",
-  },
-  {
     "hrsh7th/cmp-buffer",
   },
   {
     "hrsh7th/cmp-path",
   },
-  --{
-  --  "hrsh7th/cmp-cmdline",
-  --},
-  --{
-  --  "hrsh7th/cmp-emoji",
-  --},
   {
     "hrsh7th/nvim-cmp",
     config = function()
@@ -27,14 +18,9 @@ return {
 
       cmp.setup({
         formatting = {
-          format = function(entry, vim_item)
+          format = function(_, vim_item)
             vim_item.menu = nil
             return vim_item
-          end,
-        },
-        snippet = {
-          expand = function(args)
-            vim.fn["vsnip#anonymous"](args.body)
           end,
         },
         mapping = {
@@ -42,8 +28,7 @@ return {
           ["<C-n>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
           ["<C-k>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
           ["<C-j>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
-          ["<C-u>"] = cmp.mapping.scroll_docs(-4),
-          ["<C-d>"] = cmp.mapping.scroll_docs(4),
+          ["<esc>"] = cmp.mapping.abort(),
           ["<C-y>"] = cmp.mapping.confirm({ select = true }),
           ["<C-Space>"] = cmp.mapping.complete(),
         },
@@ -51,8 +36,6 @@ return {
           { name = "nvim_lsp" },
           { name = "path" },
           { name = "buffer" },
-          --{ name = 'cmdline' },
-          --{ name = 'emoji' },
           { name = "crates" },
         },
         window = {
