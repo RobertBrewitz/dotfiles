73d5bedbaca808c1e67927455c7d146ca4fb66d2
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2024-05-12 01:20:01 +0200

    IMPROVES rustaceanvim config

--- nvim/lua/jrbb/plugins/lsp/rust.lua
+++ nvim/lua/jrbb/plugins/lsp/rust.lua
@@ -2,8 +2,40 @@ return {
   {
     "mrcjkb/rustaceanvim",
     version = "^4",
-    lazy = false,
     ft = { "rust" },
+    opts = {
+      server = {
+        default_settings = {
+          -- rust-analyzer language server configuration
+          ["rust-analyzer"] = {
+            cargo = {
+              allFeatures = true,
+              loadOutDirsFromCheck = true,
+              buildScripts = {
+                enable = true,
+              },
+            },
+            -- Add clippy lints for Rust.
+            checkOnSave = {
+              allFeatures = true,
+              command = "clippy",
+              extraArgs = { "--no-deps" },
+            },
+            procMacro = {
+              enable = true,
+              ignored = {
+                ["async-trait"] = { "async_trait" },
+                ["napi-derive"] = { "napi" },
+                ["async-recursion"] = { "async_recursion" },
+              },
+            },
+          },
+        },
+      },
+    },
+    config = function(_, opts)
+      vim.g.rustaceanvim = vim.tbl_deep_extend("keep", vim.g.rustaceanvim or {}, opts or {})
+    end,
   },
   {
     "saecki/crates.nvim",
