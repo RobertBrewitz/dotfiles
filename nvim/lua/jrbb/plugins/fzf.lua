3b6b8e048aaf15d8d134a689f1c491d1983434ad
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2024-06-11 09:49:35 +0200

    UPDATES fzf lazy config

--- nvim/lua/jrbb/plugins/fzf.lua
+++ nvim/lua/jrbb/plugins/fzf.lua
@@ -1,11 +1,17 @@
 return {
-  "junegunn/fzf.vim",
-  dependencies = "junegunn/fzf",
-  config = function()
-    local Remap = require("jrbb.keymap")
-    local nnoremap = Remap.nnoremap
+  {
+    "junegunn/fzf",
+    build = "./install --all"
+  },
+  {
+    "junegunn/fzf.vim",
+    dependencies = "junegunn/fzf",
+    config = function()
+      local Remap = require("jrbb.keymap")
+      local nnoremap = Remap.nnoremap
 
-    nnoremap("<c-p>", ":Files<cr>")
-    nnoremap("<leader>p", ":Files<cr>")
-  end,
+      nnoremap("<c-p>", ":Files<cr>")
+      nnoremap("<leader>p", ":Files<cr>")
+    end,
+  }
 }
