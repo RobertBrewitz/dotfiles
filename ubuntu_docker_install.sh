a9d794b2c054467bc677adc8bfa4e70ad93b7a88
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2024-10-23 11:22:50 +0200

    UPDATES ubuntu docker install

--- ubuntu_docker_install.sh
+++ ubuntu_docker_install.sh
@@ -3,12 +3,13 @@
 # https://docs.docker.com/engine/install/ubuntu/
 echo "Setting up apt Docker repository"
 sudo apt-get update
-sudo apt-get install ca-certificates curl gnupg -y
-sudo mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 echo \
-  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
@@ -22,18 +23,18 @@ if ! id -nG "$USER" | grep -qw "docker"; then
   newgrp docker
 fi
 
-echo "Make Docker use host machine's DNS"
-sudo apt-get install dnsmasq
-sudo mkdir /etc/resolvconf/resolv.conf.d
-sudo touch /etc/resolvconf/resolv.conf.d/tail
-echo "nameserver 172.17.0.1" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
-sudo touch /etc/NetworkManager/dnsmasq.d/docker.conf
-sudo tee -a /etc/NetworkManager/dnsmasq.d/docker.conf <<EOF
-interface=lo
-interface=tun0
-interface=docker0
-listen-address=172.17.0.1
-bind-dynamic
-EOF
+# echo "Make Docker use host machine's DNS"
+# sudo apt-get install dnsmasq
+# sudo mkdir /etc/resolvconf/resolv.conf.d
+# sudo touch /etc/resolvconf/resolv.conf.d/tail
+# echo "nameserver 172.17.0.1" | sudo tee -a /etc/resolvconf/resolv.conf.d/tail
+# sudo touch /etc/NetworkManager/dnsmasq.d/docker.conf
+# sudo tee -a /etc/NetworkManager/dnsmasq.d/docker.conf <<EOF
+# interface=lo
+# interface=tun0
+# interface=docker0
+# listen-address=172.17.0.1
+# bind-dynamic
+# EOF
 
 echo "DONE"
