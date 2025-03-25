c7aae6c302a0e47c2cd8bacbc7e69adc6ffdea35
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-03-25 20:45:10 +0100

    set moonfly theme background to transparent

--- nvim/lua/jrbb/plugins/colors.lua
+++ nvim/lua/jrbb/plugins/colors.lua
@@ -2,6 +2,7 @@ return {
   "bluz71/vim-moonfly-colors",
   config = function()
     vim.g.jrbb_colorscheme = "moonfly"
+    vim.g.moonflyTransparent = true
 
     function ApplyColorscheme()
       vim.cmd("colorscheme " .. vim.g.jrbb_colorscheme)
