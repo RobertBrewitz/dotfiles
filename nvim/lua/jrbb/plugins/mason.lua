df13a8b4da44f0b4f29ade3f757a4df8b16a6edc
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-06-01 21:44:22 +0200

    ENABLES mason without rust_analyzer

--- nvim/lua/jrbb/plugins/mason.lua
+++ nvim/lua/jrbb/plugins/mason.lua
@@ -1,50 +1,49 @@
 return {
-  -- {
-  --   "williamboman/mason.nvim",
-  --   opts = function()
-  --     return {
-  --       ui = {
-  --         icons = {
-  --           package_installed = "✓",
-  --           package_pending = "➜",
-  --           package_uninstalled = "✗",
-  --         },
-  --       },
-  --     }
-  --   end,
-  --   config = function(_, opts)
-  --     require("mason").setup(opts)
-  --   end,
-  -- },
-  -- {
-  --   "WhoIsSethDaniel/mason-tool-installer.nvim",
-  --   opts = function()
-  --     return {
-  --       ensure_installed = {
-  --         "eslint_d",
-  --         "prettier",
-  --       },
-  --     }
-  --   end,
-  --   config = function(_, opts)
-  --     require("mason-tool-installer").setup(opts)
-  --   end,
-  -- },
-  -- {
-  --   "williamboman/mason-lspconfig.nvim",
-  --   opts = function()
-  --     return {
-  --       ensure_installed = {
-  --         "rust_analyzer",
-  --         "wgsl_analyzer",
-  --         "ts_ls",
-  --         "lua_ls",
-  --         "eslint",
-  --       },
-  --     }
-  --   end,
-  --   config = function(_, opts)
-  --     require("mason-lspconfig").setup(opts)
-  --   end,
-  -- },
+  {
+    "williamboman/mason.nvim",
+    opts = function()
+      return {
+        ui = {
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗",
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mason").setup(opts)
+    end,
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    opts = function()
+      return {
+        ensure_installed = {
+          "eslint_d",
+          "prettier",
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mason-tool-installer").setup(opts)
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = function()
+      return {
+        ensure_installed = {
+          "wgsl_analyzer",
+          "ts_ls",
+          "lua_ls",
+          "eslint",
+        },
+      }
+    end,
+    config = function(_, opts)
+      require("mason-lspconfig").setup(opts)
+    end,
+  },
 }
