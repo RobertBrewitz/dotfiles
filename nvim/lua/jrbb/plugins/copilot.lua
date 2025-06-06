c72a0d363ad63aec28c806ef797088b4a603ba99
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-06-06 18:24:20 +0200

    DISABLES copilot altogether, utter garbage

--- nvim/lua/jrbb/plugins/copilot.lua
+++ nvim/lua/jrbb/plugins/copilot.lua
@@ -1,12 +1,12 @@
 return {
-  "github/copilot.vim",
-  config = function()
-    local Remap = require("jrbb.keymap")
-    local nnoremap = Remap.nnoremap
-
-    nnoremap("<leader>cp", ":Copilot panel<cr>", { desc = "Copilot panel" })
-    nnoremap("<leader>cs", ":Copilot status<cr>", { desc = "Copilot status" })
-    nnoremap("<leader>cd", ":Copilot disable<cr>", { desc = "Copilot disable" })
-    nnoremap("<leader>ce", ":Copilot enable<cr>", { desc = "Copilot enable" })
-  end,
+  -- "github/copilot.vim",
+  -- config = function()
+  --   local Remap = require("jrbb.keymap")
+  --   local nnoremap = Remap.nnoremap
+  --
+  --   nnoremap("<leader>cp", ":Copilot panel<cr>", { desc = "Copilot panel" })
+  --   nnoremap("<leader>cs", ":Copilot status<cr>", { desc = "Copilot status" })
+  --   nnoremap("<leader>cd", ":Copilot disable<cr>", { desc = "Copilot disable" })
+  --   nnoremap("<leader>ce", ":Copilot enable<cr>", { desc = "Copilot enable" })
+  -- end,
 }
