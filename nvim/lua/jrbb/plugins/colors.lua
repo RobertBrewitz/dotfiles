ba94dcdcdb400e52f6523b3f41cf8844a5a0c3a4
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-10-15 14:08:10 +0200

    cleanup

--- nvim/lua/jrbb/plugins/colors.lua
+++ nvim/lua/jrbb/plugins/colors.lua
@@ -49,15 +49,4 @@ return {
       ApplyColorscheme()
     end,
   },
-  -- "bluz71/vim-moonfly-colors",
-  -- config = function()
-  --   vim.g.jrbb_colorscheme = "moonfly"
-  --   vim.g.moonflyTransparent = true
-  --
-  --   function ApplyColorscheme()
-  --     vim.cmd("colorscheme " .. vim.g.jrbb_colorscheme)
-  --   end
-  --
-  --   ApplyColorscheme()
-  -- end,
 }
--- nvim/lua/jrbb/plugins/conform.lua
+++ nvim/lua/jrbb/plugins/conform.lua
@@ -25,38 +25,3 @@ return {
     })
   end,
 }
--- return {
---   "sbdchd/neoformat",
---   config = function()
---     local autocmd = vim.api.nvim_create_autocmd
---
---     autocmd({ "BufWritePre" }, {
---       command = "Neoformat",
---       pattern = "*.rs",
---     })
---
---     autocmd({ "BufWritePre" }, {
---       command = "Neoformat",
---       pattern = "*.{t,j}s",
---     })
---
---     autocmd({ "BufWritePre" }, {
---       command = "Neoformat",
---       pattern = "*.{t,j}sx",
---     })
---
---     autocmd({ "BufWritePre" }, {
---       command = "Neoformat",
---       pattern = "*.json",
---     })
---
---     autocmd({ "BufWritePre" }, {
---       command = "Neoformat",
---       pattern = "*.md",
---     })
---
---     vim.g.neoformat_try_node_exe = 1
---     vim.g.neoformat_enabled_javascript = { "prettier" }
---     vim.g.neoformat_run_all_formatters = 1
---   end,
--- }
