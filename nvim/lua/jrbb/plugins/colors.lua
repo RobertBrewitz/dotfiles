66b8d6cee7cfcd7f3b8c294537b6e6429bd5884c
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-10-15 13:53:13 +0200

    change to catppuccin to keep it simple

--- nvim/lua/jrbb/plugins/colors.lua
+++ nvim/lua/jrbb/plugins/colors.lua
@@ -1,13 +1,63 @@
 return {
-  "bluz71/vim-moonfly-colors",
-  config = function()
-    vim.g.jrbb_colorscheme = "moonfly"
-    vim.g.moonflyTransparent = true
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      vim.g.jrbb_colorscheme = "catppuccin-mocha"
 
-    function ApplyColorscheme()
-      vim.cmd("colorscheme " .. vim.g.jrbb_colorscheme)
-    end
+      function ApplyColorscheme()
+        vim.cmd("colorscheme " .. vim.g.jrbb_colorscheme)
+      end
 
-    ApplyColorscheme()
-  end,
+      require("catppuccin").setup({
+        transparent_background = true,
+        default_integrations = false,
+        integrations = {
+          gitsigns = true,
+          nvimtree = true,
+          telescope = true,
+          treesitter = true,
+          which_key = true,
+          mason = true,
+          notify = true,
+          copilot_vim = true,
+          blink_cmp = {
+            style = "bordered",
+          },
+        },
+        custom_highlights = function(colors)
+          return {
+            WinSeparator = { fg = colors.overlay1 },
+            BlinkCmpDocBorder = { fg = colors.blue },
+            BlinkCmpKind = { fg = colors.blue },
+            BlinkCmpMenu = { fg = colors.text },
+            BlinkCmpMenuBorder = { fg = colors.blue, bg = colors.base },
+            BlinkCmpSignatureHelpActiveParameter = { fg = colors.mauve },
+            BlinkCmpSignatureHelpBorder = { fg = colors.blue },
+          }
+        end,
+        color_overrides = {
+          mocha = {
+            base = "#000000",
+            mantle = "#000000",
+            crust = "#000000",
+          },
+        },
+      })
+
+      ApplyColorscheme()
+    end,
+  },
+  -- "bluz71/vim-moonfly-colors",
+  -- config = function()
+  --   vim.g.jrbb_colorscheme = "moonfly"
+  --   vim.g.moonflyTransparent = true
+  --
+  --   function ApplyColorscheme()
+  --     vim.cmd("colorscheme " .. vim.g.jrbb_colorscheme)
+  --   end
+  --
+  --   ApplyColorscheme()
+  -- end,
 }
