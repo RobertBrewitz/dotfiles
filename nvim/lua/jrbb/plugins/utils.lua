1f06e526d0ea7bca18d8b624812ea1fc8d191621
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-10-15 14:03:45 +0200

    use nvim-surround instead of vim-surround

--- nvim/lua/jrbb/plugins/utils.lua
+++ nvim/lua/jrbb/plugins/utils.lua
@@ -1,6 +1,11 @@
 return {
   {
-    "tpope/vim-surround",
+    "kylechui/nvim-surround",
+    version = "^3.0.0",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
   },
   {
     "tpope/vim-repeat",
