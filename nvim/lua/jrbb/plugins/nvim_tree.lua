3f6db4ad2319d29512aaaaeff963194345a9bdf2
Jan Robert Brewitz Borg <hello@robertbrewitz.com>
2025-10-21 14:24:17 +0200

    add .git to nvim_tree custom filter

--- nvim/lua/jrbb/plugins/nvim_tree.lua
+++ nvim/lua/jrbb/plugins/nvim_tree.lua
@@ -116,6 +116,7 @@ return {
         },
         filters = {
           dotfiles = false,
+          custom = { ".git" },
         },
         on_attach = function(bufnr)
           local api = require("nvim-tree.api")
