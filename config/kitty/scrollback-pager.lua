18586744b8e3a332afd4c4aee0063acf20b458a4
Robert Borg <hello@robertbrewitz.com>
2026-01-25 15:25:13 +0100

    scrollback with nvim with ansi color sequences

--- config/kitty/scrollback-pager.lua
+++ config/kitty/scrollback-pager.lua
@@ -7,8 +7,23 @@ vim.opt.clipboard = "unnamedplus"
 
 vim.keymap.set("n", "q", "<cmd>qa!<cr>")
 
+local baleia = require("baleia").setup()
+
+local function strip_osc(line)
+  -- OSC sequences (ESC ] ... BEL or ESC ] ... ST)
+  line = line:gsub("\027%][^\007\027]*\007", "")
+  line = line:gsub("\027%][^\027]*\027\\", "")
+  return line
+end
+
 vim.api.nvim_create_autocmd("VimEnter", {
   callback = function()
+    local lines = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+    for i, line in ipairs(lines) do
+      lines[i] = strip_osc(line)
+    end
+    vim.api.nvim_buf_set_lines(0, 0, -1, false, lines)
+    baleia.once(vim.api.nvim_get_current_buf())
     vim.cmd("normal G")
     vim.bo.buftype = "nofile"
     vim.bo.modifiable = false
