968c605bd6e9a9d3f8440425694ad996f11b2889
Robert Borg <hello@robertbrewitz.com>
2026-01-27 13:24:49 +0100

    voyager pairing

--- arch.sh
+++ arch.sh
@@ -185,6 +185,30 @@ yay -S --noconfirm catppuccin-gtk-theme-mocha kvantum-theme-catppuccin-git catpp
 echo "Configuring Kvantum theme"
 kvantummanager --set catppuccin-mocha-blue
 
+echo "Installing ZSA keyboard udev rules"
+sudo groupadd -f plugdev
+sudo usermod -aG plugdev $USER
+cat << 'EOF' | sudo tee /etc/udev/rules.d/50-zsa.rules
+# Rules for Oryx web flashing and live training
+KERNEL=="hidraw*", ATTRS{idVendor}=="16c0", MODE="0664", GROUP="plugdev"
+KERNEL=="hidraw*", ATTRS{idVendor}=="3297", MODE="0664", GROUP="plugdev"
+
+# Legacy rules for live training over webusb (Not needed for firmware v21+)
+SUBSYSTEM=="usb", ATTR{idVendor}=="3297", GROUP="plugdev"
+
+# Wally Flashing rules for the Ergodox EZ
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", ENV{ID_MM_DEVICE_IGNORE}="1"
+ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789A]?", ENV{MTP_NO_PROBE}="1"
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789ABCD]?", MODE:="0666"
+KERNEL=="ttyACM*", ATTRS{idVendor}=="16c0", ATTRS{idProduct}=="04[789B]?", MODE:="0666"
+
+# Keymapp / Wally Flashing rules for the Moonlander and Planck EZ
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="0483", ATTRS{idProduct}=="df11", MODE:="0666", SYMLINK+="stm32_dfu"
+# Keymapp Flashing rules for the Voyager
+SUBSYSTEMS=="usb", ATTRS{idVendor}=="3297", MODE:="0666", SYMLINK+="ignition_dfu"
+EOF
+sudo udevadm control --reload-rules && sudo udevadm trigger
+
 echo "Symlinking dotfiles"
 "$DOTFILES/symlink_arch.sh"
 
