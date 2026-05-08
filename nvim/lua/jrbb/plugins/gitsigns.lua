2032c93ec0f5c79ffe08c82bf32a0768c43b0f2a
Robert Borg <hello@robertbrewitz.com>
2026-05-08 08:03:54 +0200

    add git signs to nvim

---
+++ nvim/lua/jrbb/plugins/gitsigns.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add          = { text = "│" },
+        change       = { text = "│" },
+        delete       = { text = "_" },
+        topdelete    = { text = "‾" },
+        changedelete = { text = "~" },
+        untracked    = { text = "┆" },
+      },
+      signs_staged_enable = true,
+      signcolumn = true,
+    },
+  },
+}
