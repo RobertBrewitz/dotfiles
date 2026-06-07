e112f8c77282c4c403f42ec95dda6eda25b02bce
Robert Borg <hello@robertbrewitz.com>
2026-06-07 18:17:28 +0200

    tabs scroll to file

--- nvim/lua/jrbb/core/options.lua
+++ nvim/lua/jrbb/core/options.lua
@@ -47,53 +47,159 @@ vim.opt.relativenumber = false
 vim.opt.showtabline = 2
 vim.opt.laststatus = 3
 
-function _G.MyTabline()
-  local s = ''
-  for i = 1, vim.fn.tabpagenr('$') do
-    local buflist = vim.fn.tabpagebuflist(i)
-    local winnr = vim.fn.tabpagewinnr(i)
-    local bufnr = buflist[winnr]
-    local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+local function tabline_escape(text)
+  return text:gsub("%%", "%%%%")
+end
 
-    -- If current window is NvimTree, find a real file buffer in this tab
-    if ft == "NvimTree" then
-      bufnr = nil
-      for _, b in ipairs(buflist) do
-        local bft = vim.api.nvim_get_option_value("filetype", { buf = b })
-        if bft ~= "NvimTree" and bft ~= "notify" and bft ~= "screenkey" then
-          bufnr = b
-          break
-        end
-      end
-    end
+local function tabline_width(text)
+  return vim.fn.strdisplaywidth(text)
+end
 
-    local label
-    if bufnr then
-      local bufname = vim.fn.bufname(bufnr)
-      local filename = vim.fn.fnamemodify(bufname, ':t')
-      local parent = vim.fn.fnamemodify(bufname, ':p:h:t')
-      local modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
-      label = parent .. '/' .. filename
-      if filename == '' then
-        label = '[No Name]'
-      end
-      if modified then
-        label = label .. ' ●'
-      end
-    else
-      label = '[Explorer]'
-    end
-
-    if i == vim.fn.tabpagenr() then
-      s = s .. '%#TabLineSel#'
-    else
-      s = s .. '%#TabLine#'
-    end
-
-    s = s .. ' ' .. label .. ' '
+local function tabline_truncate(text, max_width)
+  if tabline_width(text) <= max_width then
+    return text
   end
 
-  s = s .. '%#TabLineFill#'
+  if max_width <= 1 then
+    return "…"
+  end
+
+  local ellipsis = "…"
+  local text_width = 0
+  local truncated = ""
+  local keep_width = max_width - tabline_width(ellipsis)
+
+  for i = 0, vim.fn.strchars(text) - 1 do
+    local char = vim.fn.strcharpart(text, i, 1)
+    local char_width = tabline_width(char)
+    if text_width + char_width > keep_width then
+      break
+    end
+    truncated = truncated .. char
+    text_width = text_width + char_width
+  end
+
+  return truncated .. ellipsis
+end
+
+local function tabline_label(bufnr)
+  if bufnr then
+    local bufname = vim.fn.bufname(bufnr)
+    local filename = vim.fn.fnamemodify(bufname, ":t")
+    local parent = vim.fn.fnamemodify(bufname, ":p:h:t")
+    local modified = vim.api.nvim_get_option_value("modified", { buf = bufnr })
+    local label = parent .. "/" .. filename
+
+    if filename == "" then
+      label = "[No Name]"
+    end
+    if modified then
+      label = label .. " ●"
+    end
+
+    return label
+  end
+
+  return "[Explorer]"
+end
+
+local function tabline_tab_bufnr(tabnr)
+  local buflist = vim.fn.tabpagebuflist(tabnr)
+  local winnr = vim.fn.tabpagewinnr(tabnr)
+  local bufnr = buflist[winnr]
+  local ft = vim.api.nvim_get_option_value("filetype", { buf = bufnr })
+
+  -- If current window is NvimTree, find a real file buffer in this tab
+  if ft == "NvimTree" then
+    for _, b in ipairs(buflist) do
+      local bft = vim.api.nvim_get_option_value("filetype", { buf = b })
+      if bft ~= "NvimTree" and bft ~= "notify" and bft ~= "screenkey" then
+        return b
+      end
+    end
+
+    return nil
+  end
+
+  return bufnr
+end
+
+function _G.MyTabline()
+  local tab_count = vim.fn.tabpagenr("$")
+  local current_tab = vim.fn.tabpagenr()
+  local columns = math.max(vim.o.columns, 1)
+  local hidden_left = " … "
+  local hidden_right = " … "
+  local hidden_left_width = tabline_width(hidden_left)
+  local hidden_right_width = tabline_width(hidden_right)
+  local tabs = {}
+
+  for i = 1, tab_count do
+    local text = " " .. tabline_label(tabline_tab_bufnr(i)) .. " "
+    tabs[i] = {
+      text = text,
+      width = tabline_width(text),
+    }
+  end
+
+  local function range_width(first, last)
+    local width = 0
+    if first > 1 then
+      width = width + hidden_left_width
+    end
+    for i = first, last do
+      width = width + tabs[i].width
+    end
+    if last < tab_count then
+      width = width + hidden_right_width
+    end
+    return width
+  end
+
+  -- The active tab must always fit; if one tab label is too wide, shorten it.
+  local active_overhead = (current_tab > 1 and hidden_left_width or 0)
+    + (current_tab < tab_count and hidden_right_width or 0)
+  local active_max_width = math.max(columns - active_overhead, 1)
+  tabs[current_tab].text = tabline_truncate(tabs[current_tab].text, active_max_width)
+  tabs[current_tab].width = tabline_width(tabs[current_tab].text)
+
+  local first = current_tab
+  local last = current_tab
+
+  while first > 1 or last < tab_count do
+    local can_add_left = first > 1 and range_width(first - 1, last) <= columns
+    local can_add_right = last < tab_count and range_width(first, last + 1) <= columns
+
+    if not can_add_left and not can_add_right then
+      break
+    end
+
+    if can_add_left and (not can_add_right or (current_tab - first) <= (last - current_tab)) then
+      first = first - 1
+    else
+      last = last + 1
+    end
+  end
+
+  local s = ""
+  if first > 1 then
+    s = s .. "%#TabLine#" .. tabline_escape(hidden_left)
+  end
+
+  for i = first, last do
+    if i == current_tab then
+      s = s .. "%#TabLineSel#"
+    else
+      s = s .. "%#TabLine#"
+    end
+    s = s .. tabline_escape(tabs[i].text)
+  end
+
+  if last < tab_count then
+    s = s .. "%#TabLine#" .. tabline_escape(hidden_right)
+  end
+
+  s = s .. "%#TabLineFill#"
   return s
 end
 
